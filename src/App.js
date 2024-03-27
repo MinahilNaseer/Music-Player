708695d7cd3651d2cb83f32fa3eb4097b158d19d
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2024-03-27 14:48:14 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/note-removebg.png';
+import wave from './assets/sound_wave-removebg.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full-screen">
+      <Navbar />
+      <ImageWithText />
     </div>
   );
+};
+
+const Navbar = () => {
+  return (
+    <nav className="topnav">
+      <div className="logo-container">
+        <img src={logo} alt="Music" />
+        <h1>Music</h1>
+      </div>
+      <div className="login-container">
+        <button>Sign Up</button>
+        <button>Log In</button>
+      </div>
+    </nav>
+  );
+};
+
+const ImageWithText = () => {
+  return (
+    <div className="image-container">
+      <div className="text-content">
+        <div className="line">
+          <h2>Music Is Your </h2>
+          <img src={wave} alt='wave' />
+        </div>
+        <div className="line">
+          <img src={wave} alt='wave' />
+          <h2>Time Machine</h2>
+        </div>
+        <button>Start your search</button>
+      </div>
+    </div>
+  );
+
 }
 
 export default App;
