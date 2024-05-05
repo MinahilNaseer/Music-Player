ccb061771b6fdfb84736bf141bbd51819a636420
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2024-05-05 20:41:30 +0500

    music player scroller is not finished

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -17,6 +17,9 @@ import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import RecommendIcon from "@mui/icons-material/Recommend";
 import SearchIcon from "@mui/icons-material/Search";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import MusicPlayer from "./musicplayer";
+//import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const DashBoard = () => {
   const { collapseSidebar } = useProSidebar();
@@ -188,6 +191,92 @@ const DashBoard = () => {
             </div>
           </div>
         </div>
+        <section class="trending">
+          <div class="trending-content">
+            <div class="trending-info">
+              <h4>Trending New Hits</h4>
+              <h5>In My Feelings</h5>
+              <p>Camila Cabello 63million Plays</p>
+              <button>Listen Now</button>
+              <FavoriteIcon
+                style={{
+                  marginLeft: "40px",
+                  color: "rgb(91,118,255)",
+                  cursor: "pointer",
+                  width: "30px",
+                  height: "30px",
+                }}
+                onMouseEnter={(e) => (e.target.style.color = "red")}
+                onMouseLeave={(e) => (e.target.style.color = "rgb(91,118,255)")}
+              />
+            </div>
+            <img
+              className="CC"
+              src="./assets/Camila.png"
+              alt="Camila Cabello"
+            />
+          </div>
+        </section>
+        <section className="top-artist">
+          <h4>Top Artists</h4>
+          <button className="see-all">See All</button>
+          <div className="square">
+            <div className="artist-info">
+              <img
+                src="./assets/Theweek.jpg"
+                alt="theweekend"
+                className="artist-image"
+              />
+              <h5>Artist Name</h5>
+              <p>Number of Listens</p>
+            </div>
+          </div>
+        </section>
+        <section className="top-charts">
+          <h4>Top Charts</h4>
+          <button className="see-all">See All</button>
+          <div class="chart-item">
+            <div class="left-chart">
+              <div class="chart-info">
+                <p>01</p>
+                <img
+                  src="./assets/Theweek.jpg"
+                  alt="theweekend"
+                  className="chart-image"
+                />
+                <div class="song-details">
+                  <h6>Song Title</h6>
+                  <p>Artist Name</p>
+                </div>
+                <p>3:45</p>
+                <PlayCircleIcon style={{color: "rgb(91,118,255)",cursor:"pointer"}} 
+                onMouseEnter={(e) => (e.target.style.color = "red")}
+                onMouseLeave={(e) => (e.target.style.color = "rgb(91,118,255)")}/>
+              </div>
+            </div>
+            <div class="divider"></div>
+            <div class="right-chart">
+              <div class="chart-info">
+                <p>02</p>
+                <img
+                  src="./assets/Theweek.jpg"
+                  alt="theweekend"
+                  className="chart-image"
+                />
+                <div class="song-details">
+                  <h6>Song Title</h6>
+                  <p>Artist Name</p>
+                  
+                </div>
+                <p>3:45</p>
+                <PlayCircleIcon style={{color: "rgb(91,118,255)",cursor:"pointer"}} 
+                onMouseEnter={(e) => (e.target.style.color = "red")}
+                onMouseLeave={(e) => (e.target.style.color = "rgb(91,118,255)")}/>
+              </div>
+            </div>
+          </div>
+        </section>
+        <MusicPlayer/>
       </main>
     </div>
   );
