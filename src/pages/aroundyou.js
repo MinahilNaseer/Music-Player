f9bfb8d708669cd1991c1354838e458b70341fca
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2024-05-12 10:35:10 +0500

    Added top charts sample component

--- src/pages/aroundyou.js
+++ src/pages/aroundyou.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import Sidenavbar from '../components/sidenavbar';
-import DashboardTopNav from '../components/dashboardtopnav';
-import '../pages/dashboard.css'
+import React from "react";
+import Sidenavbar from "../components/sidenavbar";
+import DashboardTopNav from "../components/dashboardtopnav";
+import "../pages/dashboard.css";
 import "../pages/topartist.css";
-import BottomPlayer from '../components/bottomplayer';
+import BottomPlayer from "../components/bottomplayer";
 
 const Aroundyou = () => {
   return (
     <div className="dashboard">
-      <Sidenavbar activePage = "/aroundyou"/>
+      <Sidenavbar activePage="/aroundyou" />
       <main>
-      <DashboardTopNav />
-      <h1>Around You In </h1>
+        <DashboardTopNav />
+        <h1>Around You In </h1>
         <div className="artist-container">
-        <section className="around-you-sec">
-          <div className="artist-cover">
-            <img src="./assets/Theweek.jpg" alt="artist-img" />
-          </div>
-          <h2 className="artist-name">The Weekend</h2>
-          <h2 className="artist-song">Star Boy</h2>
-          <img className='play-icon' src='./assets/playicon-remove.png' alt='icon'/>
-        </section>
+          <section className="around-you-sec">
+            <div className="artist-cover">
+              <img src="./assets/Theweek.jpg" alt="artist-img" />
+            </div>
+            <h2 className="artist-name">The Weekend</h2>
+            <h2 className="artist-song">Star Boy</h2>
+            <img
+              className="play-icon"
+              src="./assets/playicon-remove.png"
+              alt="icon"
+            />
+          </section>
         </div>
       </main>
-      <BottomPlayer/>
+      <BottomPlayer />
     </div>
-  )
-}
+  );
+};
 
-export default Aroundyou
+export default Aroundyou;
