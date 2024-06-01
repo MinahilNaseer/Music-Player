722bcae5d84cf36f6ac06cb51746f2b21c0e9a6e
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2024-06-02 00:42:22 +0500

    small addition and changes

--- src/components/sidenavbar.js
+++ src/components/sidenavbar.js
@@ -43,6 +43,9 @@ const Sidenavbar = () => {
       setIsSidebarExpanded(false);
     }
   };
+  const handleSignOutClick=()=>{
+    navigate("/")
+  }
 
   return (
     <>
@@ -158,7 +161,7 @@ const Sidenavbar = () => {
         <button
           type="button"
           className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-          onClick={() => navigate('/signout')}
+          onClick={handleSignOutClick}
         >
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Sign Out
